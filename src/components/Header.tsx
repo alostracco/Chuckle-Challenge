788c06fba34b21e9b1525d6c2fba51c6c99e67c9
Bruce4PF <bliu445@uwo.ca>
2023-06-19 21:41:40 -0400

    Made the logo scroll to top instead of instant

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,32 +4,39 @@ import ThemeToggle from "./ThemeToggle";
 import { FaGithub } from "react-icons/fa";
 
 const Header = () => {
-return (
-    <Box
-    position="fixed"
-    top="0"
-    zIndex="999999999"
-    w="full"
-    >
-    <Container maxWidth="container.lg" pl={4} pr={4} pt={2} pb={2}>
+  const handleLogoClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <Box position="fixed" top="0" zIndex="999999999" w="full">
+      <Container maxWidth="container.lg" pl={4} pr={4} pt={2} pb={2}>
         <Flex alignItems="center" h="3.5rem">
-            <Center as={Link} href="#top">
-                <Image 
-                    src="../Chuckle Challenge v2 Logo.png"
-                    height="30"
-                />
-            </Center>
-            <Spacer />
-            <Flex gap='3'>
+          <Center>
+            <Image
+              src="../Chuckle Challenge v2 Logo.png"
+              height="30"
+              onClick={handleLogoClick}
+              style={{ cursor: "pointer" }}
+            />
+          </Center>
+          <Spacer />
+          <Flex gap="3">
             <ThemeToggle />
             <ButtonGroup variant="tertiary">
-                <IconButton as="a" href="https://github.com/alostracco/Try-Not-To-Smile-Challenge" target="_blank" aria-label="GitHub" icon={<FaGithub fontSize="1.25rem" />} />
+              <IconButton
+                as="a"
+                href="https://github.com/alostracco/Try-Not-To-Smile-Challenge"
+                target="_blank"
+                aria-label="GitHub"
+                icon={<FaGithub fontSize="1.25rem" />}
+              />
             </ButtonGroup>
-            </Flex>
+          </Flex>
         </Flex>
-    </Container>
+      </Container>
     </Box>
-);
+  );
 };
 
 export default Header;
