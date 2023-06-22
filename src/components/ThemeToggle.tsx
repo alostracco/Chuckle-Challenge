495d94913b0606623a1c3f4d494a17ebc1252fb5
Bruce4PF <bliu445@uwo.ca>
2023-06-22 16:46:13 -0400

    Built Custom Button

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,15 +1,27 @@
-import { IconButton, useColorMode } from '@chakra-ui/react';
+import { IconButton, chakra, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
 import { RiMoonFill, RiSunLine } from 'react-icons/ri';
 
 const ThemeToggle = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const CustomBg = useColorModeValue('rgba(251,211,141,0.5)', 'whiteAlpha.200');
+  const CustomHoverBg = useColorModeValue('rgba(251,211,141,0.8)', 'whiteAlpha.300');
+
+  const CustomIconButton = chakra(IconButton, {
+    baseStyle: {
+      _hover: {
+        bg: CustomHoverBg,
+        bgOpacity: '0%',
+      }
+    }
+  });
 
   return (
-    <IconButton
+    <CustomIconButton
       aria-label="theme toggle"
       icon={colorMode === 'light' ? <RiMoonFill /> : <RiSunLine />}
       onClick={toggleColorMode}
+      bg={CustomBg}
     />
   );
 };
