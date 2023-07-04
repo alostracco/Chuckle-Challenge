2d57980413893a293b598987a2b2895ccec7aadf
Aidan Matthew LoStracco <alostrac@uwo.ca>
2023-07-03 21:00:43 -0400

    adding flask apii to connect model to front end

---
+++ src/pages/api/FER_API.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import base64
+import io
+import numpy as np
+from tensorflow import keras
+from flask import Flask, request, jsonify
+from PIL import Image
+
+app = Flask(__name__)
+
+# Define facial emotion recognition model
+def facial_emotion_recognition(image_base64):
+    base64_decoded = base64.b64decode(image_base64)
+
+    image = Image.open(io.BytesIO(base64_decoded))
+    image_array = np.array(image)
+
+
+    model = tf.keras.models.load_model('FER.h5')
+
+    image_array = image_array.reshape((1, ) + image_array.shape)
+
+    prediction = model.predict(image_array)
+
+    return prediction.index(max(prediction)) 
+
+@app.route('/api/emotion', methods=['POST'])
+def recognize_emotion():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file found.'}), 400
+
+    image = request.files['image']
+    # Process the image here using facial emotion recognition model
+    predicted_emotion = facial_emotion_recognition(image)
+
+    return jsonify({'emotion': predicted_emotion})
+
+if __name__ == '__main__':
+    app.run(debug=True)
