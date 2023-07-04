4ec1cbace9d5e0e538ee594010fba22e9a6d03d3
Aidan LoStracco <106183667+alostracco@users.noreply.github.com>
2023-07-04 10:14:15 -0400

    Updated fetch address

--- src/components/Webcam.tsx
+++ src/components/Webcam.tsx
@@ -88,7 +88,8 @@ const WebcamFeed = () => {
       // Pass the `imageSrc` to machine learning model for processing
 
       // Make a POST request to the Flask API
-      fetch('http://localhost:3000/api/emotion', {
+      // Might need to change address to 'http://localhost:3000/api/emotion'
+      fetch('/api/emotion', {
         method: 'POST',
         body: imageSrc,
       })
