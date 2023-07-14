d1f71f4fd9dbc418e9c6c98c77f20fc62c172311
Bruce4PF <bliu445@uwo.ca>
2023-07-14 17:53:43 -0400

    Change Directory Structure for Vercel API support

--- src/components/Webcam.tsx
+++ src/components/Webcam.tsx
@@ -94,7 +94,7 @@ const WebcamFeed = () => {
       const imageSrc = webcamRef.current.getScreenshot();
 
       // Make an HTTP POST request to the Flask API
-      fetch('http://localhost:5000/predict', {
+      fetch('https://chuckle-challenge.vercel.app/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
