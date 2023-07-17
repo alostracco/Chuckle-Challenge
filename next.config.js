d3497f06da2ac4c7c94ea78d2e8d3b98700c7841
Bruce4PF <bliu445@uwo.ca>
2023-07-17 18:18:37 -0400

    Bypass CORS restrictions

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig, {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://chucklechallengebackend.vercel.app//api/:path*',
+      },
+    ];
+  },
+};
