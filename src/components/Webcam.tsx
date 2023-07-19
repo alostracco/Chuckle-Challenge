cefc43c0f05530800c5bad86a2e41d4bc71838c8
Bruce4PF <bliu445@uwo.ca>
2023-07-19 18:27:54 -0400

    Update fetch to public IP

--- src/components/Webcam.tsx
+++ src/components/Webcam.tsx
@@ -94,7 +94,7 @@ const WebcamFeed = () => {
       const imageSrc = webcamRef.current.getScreenshot();
 
       // Make an HTTP POST request to the Flask API backend
-      fetch('https://ec2-3-14-9-79.us-east-2.compute.amazonaws.com/predict', {
+      fetch('https://3.14.9.79/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
