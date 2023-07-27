02bb908486c15da51fea312be56f26eb39cb008b
Bruce4PF <bliu445@uwo.ca>
2023-07-26 21:58:23 -0400

    Connected axios instance, implemented get, and UI additions

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Box, Button, Flex, Input, chakra, useColorModeValue } from "@chakra-ui/react";
 import Hover from "@/animations/Hover";
+import youtube from "@/pages/api/youtube";
 
 interface SearchProps {
     show: boolean;
@@ -22,10 +23,21 @@ const Search: React.FC<SearchProps> = ({ show }) => {
     });
 
     const [searchInput, setSearchInput] = useState("");
+    const [videos, setVideos] = useState<any[]>([]);
 
-    const handleSearchClick = () => {
-        // Temporary until YouTube Data API v3 is connected
-        console.log(searchInput);
+    const handleSearchClick = async () => {
+        try {
+            const response = await youtube.get("/search", {
+                params: {
+                    q: searchInput
+                }
+            });
+
+            setVideos(response.data.items);
+        }
+        catch (error) {
+            console.error("Error fetching videos:", error);
+        }
     };
 
     return (
@@ -38,27 +50,47 @@ const Search: React.FC<SearchProps> = ({ show }) => {
             width='48rem'
         >
             <Flex gap={3}>
-                <Hover>
-                    <Input
-                        placeholder="Search Youtube..."
-                        _placeholder={{ opacity: 0.7, color: 'gray.800' }}
-                        focusBorderColor='transparent'
-                        borderColor="transparent"
-                        isInvalid
-                        errorBorderColor="transparent"
-                        bg={SearchBg}
-                        size="md"
-                        width='17rem'
-                        color='gray.800'
-                        fontWeight='semibold'
-                        onChange={(e) => setSearchInput(e.target.value)}
-                    />
-                </Hover>
+                <Input
+                    placeholder="Search Youtube..."
+                    _placeholder={{ opacity: 0.7, color: 'gray.800' }}
+                    focusBorderColor='transparent'
+                    borderColor="transparent"
+                    isInvalid
+                    errorBorderColor="transparent"
+                    bg={SearchBg}
+                    size="md"
+                    width='17rem'
+                    color='gray.800'
+                    fontWeight='semibold'
+                    onChange={(e) => setSearchInput(e.target.value)}
+                />
                 <Hover>
                     <CustomButton bg={SearchBg} onClick={handleSearchClick}>
                         Search
                     </CustomButton>
                 </Hover>
+
+                <Flex direction="column" mt={4}>
+                    {videos.map((video) => (
+                        <Flex key={video.id.videoId} alignItems="center" mb={3}>
+                            <a
+                                href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                style={{ marginRight: "8px" }}
+                                onClick={() => console.log(video.id.videoId)}
+                            >
+                                <img
+                                    src={video.snippet.thumbnails.default.url}
+                                    alt={video.snippet.title}
+                                    width="120"
+                                    height="90"
+                                />
+                            </a>
+                            <span>{video.snippet.title}</span>
+                        </Flex>
+                    ))}
+                </Flex>
             </Flex>
         </Box>
     );
